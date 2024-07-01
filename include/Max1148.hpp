e432d46d73c552f1828a5ca34decf04cd07f9aff
swallace23 <sean.k.wallace.27@dartmouth.edu>
2024-07-01 15:10:35 -0400

    Fixed averaging, SPI speed. Removed unnecessary functions. Changed "Command" enum to strongly typed "Channel" enum. Channel now initialized at object initialization. Moved init() to Max1148.cpp. Updated documentation.

--- include/Max1148.hpp
+++ include/Max1148.hpp
@@ -2,7 +2,10 @@
  * @file Max1148.hpp
  * @brief Header file for the Max1148 library for Dartmouth's 317 Lab. Manages ADC communication through SPI.
  * 
- *  Basically just the Max1147 library from the Isinglass/Apophis shield code rewritten for the Arduino Due. 
+ * Each Max1148 object should correspond to an ADC channel. The channel is set in the constructor. 
+ * This makes dealing with the 4 ADCs on the new set up quite simple.
+ * 
+ *  This is really just the Max1147 library from the Isinglass/Apophis shield code rewritten for the new board.
  *  Written by Jacob at some point unknown to me.
  * 
  * See the data sheet for more information - https://www.analog.com/media/en/technical-documentation/data-sheets/MAX1146-MAX1149.pdf
@@ -10,34 +13,44 @@
  * @author Sean Wallace
  * @date 2024-06-25
  */
+
+
+/*
+External clock mode is fine. Don't need to be concerned about noise. Means don't do the 8 us delay thing. 
+Noise in the data sehet has more to do with the circuitb oard layout
+
+Need to know that bc different control lines on the PC board. 
+
+There are two SPI boxes. Use one for the memory and one for the ADC. Arduino should manage on its own.
+    Does it give access to the second SPI? not that important.
+*/
 #ifndef MAX1148_HPP
 #define MAX1148_HPP
 #include <Arduino.h>
 #include <SPI.h>
 
-#define SPI_SPEED 14000000
+#define SPI_SPEED 2000000
 #define SPI_MODE SPI_MODE0
 #define ADC_CHANNEL 0
 #define ADC_PIN 10
 #define ADC_CS_PIN 10
+#define READ 0x00
 
 /**
- * @brief The commands for the Max1148 ADC.
- * READ is a dummy byte used to read the ADC.
- * CHAN0-7 are config commands to set the ADC channel.
- * As default, the ADC is set to single ended, unipolar conversion, and internal clock mode.
+ * @brief Control bytes for the Max1148 ADC.
+ * Only modifiable parameter is the channel.
+ * We use single ended, unipolar conversion, and external clock mode. Note that the old code used internal clock.
  */
-enum Command{
-    CHAN0 = 0x8E,
-    CHAN1 = 0xCE,
-    CHAN2 = 0x9E,
-    CHAN3 = 0xDE,
-    CHAN4 = 0xAE,
-    CHAN5 = 0xEE,
-    CHAN6 = 0xBE,
-    CHAN7 = 0xFE,
-    READ = 0x00
-};
+enum class Channel : uint8_t{
+    CHAN0 = 0x8F,
+    CHAN1 = 0xCF,
+    CHAN2 = 0x9F,
+    CHAN3 = 0xDF,
+    CHAN4 = 0xAF,
+    CHAN5 = 0xEF,
+    CHAN6 = 0xBF,
+    CHAN7 = 0xFF
+    };
 
 class Max1148{
     /**
@@ -45,7 +58,14 @@ class Max1148{
      */
     friend class Pip;
     private:
+        /**
+         * @brief Chip select pin for the ADC.
+         */
         int cs_pin;
+        /**
+         * @brief Channel for the ADC. Selected in the constructor.
+         */
+        uint8_t channel;
         /**
          * @brief Sets the chip select pin to low.
          */
@@ -55,30 +75,29 @@ class Max1148{
          */
         void csh();
         /**
-         * @brief Resets the ADC. Must be called within a SPI transaction
-         */
-        void clear();
-        void power_down();
-        /**
          * @brief Reads an average ADC value from the Max1148 ADC. Uses Arduino's SPI library. 
          * 
          * @param avg_num The number of samples to average.
          */
         uint16_t adc_read_avg(int avg_number);
+        /**
+         * @brief Reads a single ADC value from the Max1148 ADC. Used in adc_read_avg.
+         */
+        uint16_t adc_read();
     public:
         /**
-         * @brief ADC constructor. Sets cs_pin as per ADC_CS_PIN macro.
+         * @brief ADC constructor. Sets cs_pin as per ADC_CS_PIN macro. Chooses channel from Channel enum.
+         * @param channel The channel to read from. Format: CHAN0, CHAN1, etc.
          */
-        Max1148(): cs_pin(ADC_CS_PIN) {}
+        Max1148(Channel channel): cs_pin(ADC_CS_PIN) {
+            this->channel = static_cast<uint8_t>(channel);
+        }
 
         /**
          * @brief Initializes the ADC. Initializes SPI bus and CS pin.
          * This is only a separate function because you can't initialize the SPI bus in a constructor (for some reason).
          */
-        void init(){
-            SPI.begin();
-            pinMode(cs_pin, OUTPUT);
-        }
+        void init();
         
 };        
 
