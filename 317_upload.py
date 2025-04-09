6fc5674d6e451ec9f3b0f0bf81d40e906d3e269d
swallace23 <sean.k.wallace.27@dartmouth.edu>
2025-04-09 11:32:15 -0400

    updated gitignore, finalized upload script

--- 317_upload.py
+++ 317_upload.py
@@ -3,17 +3,22 @@
 import os
 import subprocess
 import re
+import time
 filepath = ".pio/build/due/firmware.bin"
 # bossa port format doesn't match above ls so we have to reconstruct
 mac_port = subprocess.check_output("ls /dev/tty.usb*", shell=True, text=True).strip()
-print(mac_port)
 port_num = re.findall(r'\d+', mac_port)[0]
-print(port_num)
-port = "cu.usbmodem"+str(port_num)
+port = "/dev/cu.usbmodem"+str(port_num)
 # send 1200 bps signal to enter boot loading mode
-command = "stty 1200 < /dev/"+port
-os.system(command)
+subprocess.run(["stty","-f",port,"1200"],check=True)
+# double check port
+mac_port = subprocess.check_output("ls /dev/tty.usb*", shell=True, text=True).strip()
+port_num = re.findall(r'\d+', mac_port)[0]
+# name format different for bossa
+port = "cu.usbmodem"+str(port_num)
+# wait for boot loader
+time.sleep(2)
 # upload firmware with bossac.
 # -U must be true for native port upload (bobshieldv6)
-command = "~/Library/Arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac -i -d --port="+port+" -U true -e -w -v -b "+filepath
+command = "~/Library/Arduino15/packages/arduino/tools/bossac/1.6.1-arduino/bossac --port="+port+" -U true -e -w -v -b "+filepath+" -R"
 os.system(command)
